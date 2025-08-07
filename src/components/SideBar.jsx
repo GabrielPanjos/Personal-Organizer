9e84216f7c59c9dfcd0116949061a4eb237be06d
Gabriel Pereira <pereiradosanjosgabriel@gmail.com>
2025-08-07 17:47:13 -0300

    ultima tentativa do deploy

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,6 +1,6 @@
 function Sidebar({ children }) {
   return (
-    <div className="h-full w-[280px] rounded bg-[#FFFFFF] dark:bg-[#1E1E1E] border-2 border-[#DDE3E8] dark:border-[#2E2E2E]">
+    <div className="h-full w-[280px] bg-[#FFFFFF] dark:bg-[#1E1E1E] border-2 border-[#DDE3E8] dark:border-[#2E2E2E]">
       {children}
     </div>
   );
