ee0785ff742f67ecd9245b596662d5f3c8c164a7
Gabriel Pereira <pereiradosanjosgabriel@gmail.com>
2025-08-06 22:30:03 -0300

    arrumando alguns errinhos e adicionando mais estilo...

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,6 +1,8 @@
-function SideBar() {
+function SideBar({ children }) {
   return (
-    <div className="h-full w-[250px] bg-[#FFFFFF] dark:bg-[#1E1E1E]"></div>
+    <div className="h-full w-[250px] bg-[#FFFFFF] dark:bg-[#1E1E1E] border-2 border-[#DDE3E8] dark:border-[#2E2E2E] ">
+      {children}
+    </div>
   );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,9 @@ import Button from "../components/Button";
 function Home() {
   return (
     <div className="flex gap-12 h-screen w-screen bg-[#F9FAFB] dark:bg-[#121212] text-[#2D3436] dark:text-[#ECEFF1] ">
-      <SideBar></SideBar>
+      <SideBar>
+        <div className=" w-full h-[200px] bg-[#FFFFFF] dark:bg-[#1E1E1E] border-b-2 border-[#DDE3E8] dark:border-[#2E2E2E] "></div>
+      </SideBar>
       <Button />
     </div>
   );
